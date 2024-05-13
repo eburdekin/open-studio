56830572b96a163bbf3b2b2e6fc2d050dfb2f59d
Eileen Burdekin <eburdekin@proton.me>
2024-05-12 22:27:04 -0700

    send user to homepage upon successful registration

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,6 +3,7 @@ import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
 import Layout from "../layouts/Layout";
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string;
@@ -13,6 +14,7 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const { showToast } = useAppContext();
 
   const {
@@ -53,6 +55,8 @@ const Register = () => {
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
       showToast({ message: "Registration successful!", type: "SUCCESS" });
+      // toast status saved in context, so will stay visible during/after navigating to homepage
+      navigate("/");
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: "ERROR" });
