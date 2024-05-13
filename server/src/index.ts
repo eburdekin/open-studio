de4b3efb6d65ef89984c9d7d3a154acc0f9ddeb2
Eileen Burdekin <eburdekin@proton.me>
2024-05-12 22:29:22 -0700

    update cors settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,23 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.ATLAS_URI as string);
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    // only accept requests from defined URL, and cookie must be included in request
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 // register endpoint with Express
 app.use("/api/auth", authRoutes);
