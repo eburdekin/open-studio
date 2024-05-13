2902579e81eec085d742426f740baa280208c1cf
Eileen Burdekin <eburdekin@proton.me>
2024-05-12 22:30:38 -0700

    add async function for api call to validate-token endpoint

--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -5,6 +5,8 @@ const API_BASE_URL = "http://localhost:7000";
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
+    // tells browsers to send cookies with request
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
@@ -16,4 +18,14 @@ export const register = async (formData: RegisterFormData) => {
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const validateToken = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error("Token invalid");
+  }
+  return response.json();
 };
